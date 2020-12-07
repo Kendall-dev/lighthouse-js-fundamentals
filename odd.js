62cd0fb49b2f9f4411a793e28dbc2cd3b05f910f
Kendall Nagy <kendallnagy@hotmail.com>
2020-12-07 22:28:23 +0000

    use function to check if number is odd

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd = function(num) {
+  return num % 2 !== 0
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("4 is odd: " + isOdd(4));
+console.log("13 is odd: " + isOdd(13));
+console.log("28 is odd: " + isOdd(28));
+console.log("387 is odd: " + isOdd(387));
